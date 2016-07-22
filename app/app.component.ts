5aa50f2183636d2d741c18f23fe79cc765d2b018
Jyri Högman <jyri.hogman@basware.com>
2016-07-22 15:56:32 +0300

    Styles edited

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,32 @@
 import { Component } from '@angular/core';
+import { HTTP_BINDINGS } from '@angular/http';
+
 @Component({
       selector: 'my-app',
-      templateUrl: 'app/templates/home.html'
-    })
-export class AppComponent { }
+      templateUrl: 'app/templates/home.html',
+      providers: [HTTP_BINDINGS]
+})
+
+export class AppComponent {
+    matchPattern: RegExp;
+    emailPattern: RegExp;
+    backendURL: string;
+    email: string;
+    fullname: string;
+    phone: string;
+    message: string;
+
+    constructor() {
+      this.matchPattern = new RegExp('^[^\\d &\/\\#,+()$~%.:;_*?<>{} ]+[^\\d &\/\\#,+()$~%.:;_*?<>{} ]$');
+      this.emailPattern = new RegExp('[^\d &\/\\#,+()$~%:;_*?<>{} .0-9]$');
+    }
+
+    submit() {
+        if (this.email != null && this.message != null && this.fullname != null)
+        {
+
+        }
+
+    }
+
+}
